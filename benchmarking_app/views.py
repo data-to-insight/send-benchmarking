b05826f46174099cb685fb52f11198804eb69025
William Levack Payne <108789355+WillLP-code@users.noreply.github.com>
2026-01-12 12:04:33 +0000

    Homepage not working

--- benchmarking_app/views.py
+++ benchmarking_app/views.py
@@ -1,3 +1,10 @@
+import pandas as pd
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(
+        request,
+        "benchmarking_app/views/home.html",
+    )
