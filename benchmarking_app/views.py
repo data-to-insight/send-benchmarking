56997420934f2f9900a6d28f8d599ed72eec0dd6
William Levack Payne <108789355+WillLP-code@users.noreply.github.com>
2026-01-14 11:52:28 +0000

    forms merged and connected

--- benchmarking_app/views.py
+++ benchmarking_app/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from send_benchmarking.settings import BASE_DIR
-from send_benchmarking.forms import PhaseEstablishmentChoice
+from send_benchmarking.forms import PhaseEstablishmentChoice, LAYearChoice
 
 
 def home(request):
@@ -14,29 +14,38 @@ def home(request):
     with open(BASE_DIR / "static/SEND_data/sen_phase_type_.csv") as f:
         phase_type = pd.read_csv(f)
 
-    form = PhaseEstablishmentChoice(
-        request.POST or None,
-        phase_choices={
-            phase: phase for phase in phase_type["phase_type_grouping"].unique()
-        },
-        establishment_choices={
-            establishment: establishment
-            for establishment in phase_type["type_of_establishment"].unique()
-        },
-        initial={"phase": "Total", "establishment": "Total"},
-    )
-    if form.is_valid():
-        phase_selected = form["phase"].value()
-        establishment = form["establishment"].value()
-        establishment = [establishment for establishment in establishment]
-    else:
-        phase_selected = "Total"
-        establishment = ["Total"]
+
+        form = PhaseEstablishmentChoice(
+            request.POST or None,
+            year_choices={
+                year: year for year in phase_type["time_period"].unique()
+            },
+            la_choices={la: la for la in phase_type[phase_type["la_name"].notna()]['la_name'].unique()},
+            phase_choices={
+                phase: phase for phase in phase_type["phase_type_grouping"].unique()
+            },
+            establishment_choices={
+                establishment: establishment
+                for establishment in phase_type["type_of_establishment"].unique()
+            },
+            initial={"phase": "Total", "establishment": "Total"},
+        )
+        if form.is_valid():
+            la_selected = form["la"].value()
+            year = str(form["year"].value())
+            phase_selected = form["phase"].value()
+            establishment = form["establishment"].value()
+            establishment = [establishment for establishment in establishment]
+        else:
+            la_selected = "Rochdale"
+            year = "202425"
+            phase_selected = "Total"
+            establishment = ["Total"]
 
     phase_slice = phase_type[
         (phase_type["phase_type_grouping"] == phase_selected)
         & (phase_type["type_of_establishment"].isin(establishment))
-        & (phase_type["time_period"].astype("str") == "202425")
+        & (phase_type["time_period"].astype("str") == year)
     ]
 
     plot = px.bar(
--- send_benchmarking/forms.py
+++ send_benchmarking/forms.py
@@ -17,11 +17,29 @@ class PhaseEstablishmentChoice(forms.Form):
 
     def __init__(self, *args, **kwargs):
         # Used to be able to pass year and placement choice arguments to the form.
+        la_choices = kwargs.pop("la_choices")
+        year_choices = kwargs.pop("year_choices")
         phase_choices = kwargs.pop("phase_choices")
         establishment_choices = kwargs.pop("establishment_choices")
         super(PhaseEstablishmentChoice, self).__init__(*args, **kwargs)
 
+        # Single choice select for LA.
+        self.fields["la"] = forms.ChoiceField(
+            widget=s2forms.Select2Widget,
+            label="Select LA",
+            required=True,
+            choices=la_choices,
+        )
+
         # Single choice select for year.
+        self.fields["year"] = forms.ChoiceField(
+            widget=s2forms.Select2Widget,
+            label="Select year",
+            required=True,
+            choices=year_choices,
+        )
+
+        # Single choice select for phase
         self.fields["phase"] = forms.ChoiceField(
             widget=s2forms.Select2Widget,
             label="Select Phase Type",
@@ -29,7 +47,7 @@ class PhaseEstablishmentChoice(forms.Form):
             choices=phase_choices,
         )
 
-        # Multi choice select for placement types
+        # Multi choice select for establishment types
         self.fields["establishment"] = forms.MultipleChoiceField(
             widget=s2forms.Select2MultipleWidget,
             label="Select Establishment Type(s)",
@@ -39,3 +57,40 @@ class PhaseEstablishmentChoice(forms.Form):
 
     helper = FormHelper()
     helper.add_input(Submit("submit", "Select Slices", css_class="btn-primary"))
+
+
+class LAYearChoice(forms.Form):
+    """
+    Used in analysis pages to make dropsdown widgets for LA and year selection.
+
+    Phase type and establishment type choices must be passed from views as such:
+    form = LAYearChoice(
+            la_choices=datacontainer.la_choices,
+            year_choices=datacontainer.la_choices,
+        )
+    """
+
+    def __init__(self, *args, **kwargs):
+        # Used to be able to pass year and placement choice arguments to the form.
+        la_choices = kwargs.pop("la_choices")
+        year_choices = kwargs.pop("year_choices")
+        super(LAYearChoice, self).__init__(*args, **kwargs)
+
+        # Single choice select for LA.
+        self.fields["la"] = forms.ChoiceField(
+            widget=s2forms.Select2Widget,
+            label="Select LA",
+            required=True,
+            choices=la_choices,
+        )
+
+        # Single choice select for year.
+        self.fields["year"] = forms.ChoiceField(
+            widget=s2forms.Select2Widget,
+            label="Select year",
+            required=True,
+            choices=year_choices,
+        )
+
+    helper = FormHelper()
+    helper.add_input(Submit("submit_la", "Select Slices", css_class="btn-primary"))
